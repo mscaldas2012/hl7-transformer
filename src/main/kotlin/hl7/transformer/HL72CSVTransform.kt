25fefd3114c5ca1c23a798644eb9801d34ca494c
Marcelo Caldas <mcq1@cdc.gov>
2020-03-18 16:09:29 -0400

    still a work in progress

--- src/main/kotlin/hl7/transformer/HL72CSVTransform.kt
+++ src/main/kotlin/hl7/transformer/HL72CSVTransform.kt
@@ -8,7 +8,7 @@ class HL72CSVTransform(val template: JsonObject) {
     companion object {
         //Factory Method
         fun getTransformer(configFile: String):HL72CSVTransform {
-            val configFile = javaClass.getResource(configFile).readText()
+            val configFile = HL72CSVTransform::class.java.getResource(configFile).readText()
             val parser = JsonParser()
             val jsonbody = parser.parse(configFile).asJsonObject
 
@@ -46,16 +46,21 @@ class HL72CSVTransform(val template: JsonObject) {
         targetPointer.add(newNodePointer.keySet().first(), newNodePointer.get(newNodePointer.keySet().first()))
     }
 
-
-
     private fun navigateJson(hl7Message: HL7ParseUtils, elem: JsonElement, parent: JsonElement? = null, path: String = "", attr: String = "", copyDoc: JsonElement, copyDocParent: JsonElement? =null) {
         if (elem.isJsonObject) {
             (elem as JsonObject).entrySet().map { prop -> navigateJson(hl7Message, prop.value, elem, "$path.${prop.key}", prop.key, (copyDoc as JsonObject).get(prop.key), copyDoc) }
         } else if (elem.isJsonArray) {
-            processArray(elem as JsonArray, hl7Message, path, attr, copyDocParent)
+            val map = mutableMapOf<String, Any>() //Array<out Array<String>>?>
+            processArray(elem as JsonArray, map, hl7Message, path)
+            val newArray = createNewJsonArray(map, attr)
+            if (copyDocParent is JsonObject) {
+                copyDocParent.remove(attr)
+                copyDocParent.add(attr, newArray)
+            }
+
         } else if (elem.isJsonPrimitive) {
             val prim = elem.asJsonPrimitive.asString
-            val newValue:Any? = transformVariable(hl7Message, path.substring(1), prim)
+            val newValue:Any? = transformVariable(hl7Message, prim)
 //            if (newValue is String) {
 //                if (newValue != null && !newValue.equals(prim))
 //                    if (copyDocParent!!.isJsonObject)
@@ -78,12 +83,10 @@ class HL72CSVTransform(val template: JsonObject) {
         }
     }
 
-    private fun processArray(elem: JsonArray, hl7Message: HL7ParseUtils, path: String, attr: String, parentElem: JsonElement) {
-//        val combinedArray = JsonArray()
+    private fun processArray(elem: JsonArray, mapValues: MutableMap<String, Any>, hl7Message: HL7ParseUtils, path: String) {
+//        val mapValues = mutableMapOf<String, Array<out Array<String>>?>()
         elem.forEachIndexed { idx, prop ->
-            val mapValues = mutableMapOf<String, Array<out Array<String>>?>()
-            populateArrayProperties(prop, mapValues, hl7Message, path, parentElem)
-            val newArray = createNewJsonArray(mapValues, attr)
+            populateArrayProperties(prop, mapValues, hl7Message, path)
 //            if (parentElem is JsonArray)
 //                parentElem.addAll(newArray)
 //            else if ((parentElem as JsonObject).get(attr) is JsonArray) {
@@ -92,26 +95,47 @@ class HL72CSVTransform(val template: JsonObject) {
 //            }
          //   ((parentElem.get(attr) as JsonArray) as JsonObject).add()
         }
-
-
-//        if (parentElem is JsonObject) {
-//            parentElem.remove(attr)
-//            parentElem.add(attr, combinedArray)
-////        } else if (parentElem is JsonArray) {
-////            parentElem.add(newArray)
-//        }
+//        return mapValues
     }
 
-    private fun populateArrayProperties(prop: JsonElement, map: MutableMap<String, Array<out Array<String>>?>, hl7Message: HL7ParseUtils, path: String, parentElement: JsonElement) {
+    private fun populateArrayProperties(prop: JsonElement, map: MutableMap<String, Any>, hl7Message: HL7ParseUtils, path: String) {
         if (prop.isJsonObject) {
-            (prop as JsonObject).entrySet().mapIndexed { i, pp ->
-                if (pp.value.isJsonPrimitive)
-                    transformVariable(hl7Message, path.substring(1), pp.value.asString)?.let { map.put("$path.${pp.key}", it) }
-                else if (pp.value.isJsonObject) {
-                    populateArrayProperties(pp.value, map, hl7Message, "$path.${pp.key}", parentElement)
-                } else if (pp.value.isJsonArray){
-                    println("found array")
-                    processArray(pp.value as JsonArray, hl7Message, "$path.${pp.key}", pp.key, prop)
+            (prop as JsonObject).entrySet().map{ pp ->
+                if (pp.value.isJsonPrimitive) {
+                    if (pp.value.asString.contains("->")) {
+                        val keypieces =pp.value.asString.split("->")
+                        val key =keypieces[0] + "[0]->" + keypieces[1]
+                        val children = transformVariable(hl7Message, key)
+                        val innerMap: MutableMap<String, Array<out Array<String>>?> = map.get(pp.key) as MutableMap<String, Array<out Array<String>>?>?
+                                ?: mutableMapOf()
+                        innerMap.put("${0 + 1}", children)
+                        map.put(pp.key, innerMap)
+                    } else {
+                        transformVariable(hl7Message, pp.value.asString)?.let { map.put("$path.${pp.key}", it) }
+                    }
+                } else if (pp.value.isJsonObject) {
+                    populateArrayProperties(pp.value, map, hl7Message, "$path.${pp.key}")
+                } else if (pp.value.isJsonArray) {
+//                    val childKey = path.substring(path.lastIndexOf('.') + 1, path.length) + "->${pp.key}"
+                    //val childKey = pp.key.replace("[i]", "[${i+1}]")
+//                    transformVariable(hl7Message, path.substring(1), childKey)?.forEachIndexed { i, it ->
+//                        processArray(pp.value as JsonArray, map, hl7Message, "$path.${pp.key}[${i+1}]")
+//                    }
+                    //GET ALL PARENTS:
+                    if (pp.key.indexOf("->") > 0) {
+                        val parentKey = pp.key.substring(0, pp.key.indexOf("->"))
+                        val childKey = pp.key.substring(pp.key.indexOf("->") + 2, pp.key.length)
+                        val parents = transformVariable(hl7Message, parentKey)
+                        parents?.forEachIndexed { i, it ->
+                            (pp.value as JsonArray).forEach { itt ->
+                                populateArrayProperties(itt, map, hl7Message, path)
+
+                            }
+                        }
+                    } else {
+                        val parents = transformVariable(hl7Message, pp.key)
+                        parents?.let { map.put(pp.key, it) }
+                    }
                 } else {
                     println("Not sure: ${pp.value.javaClass}")
                 }
@@ -122,31 +146,56 @@ class HL72CSVTransform(val template: JsonObject) {
         }
     }
 
-    private fun createNewJsonArray(mapValues: MutableMap<String, Array<out Array<String>>?>, attr: String): JsonArray {
+    private fun createNewJsonArray(mapValues: MutableMap<String, Any>, attr: String): JsonArray {
         //Add all Elements to array:
         val newArray = JsonArray()
-        for (index in 1..(mapValues.values.first()?.size ?: 0)) {
+        val numberOfIterations =
+                if (mapValues.values.first() is Array<*>)
+                    (mapValues.values.first() as Array<*>).size
+                else (mapValues.values.first() as MutableMap<*, *>).size
+
+
+        for (index in 1..(numberOfIterations ?: 0)) {
             val newObj = JsonObject()
             mapValues.forEach { k ->
-                val kSize = k.value?.size ?: 0
-                val innerValue = if (kSize >= index) k.value?.get(index - 1) else null
-                if (innerValue == null)
-                    newObj.add(k.key, null)
-                else when (innerValue.size) {
-                    0 -> newObj.add(k.key.substring(1).split('.').last(), null)
-                    1 -> {
-                        val bline = k.key.split('.')
-                        val tempJsonObj = createJsonTree(bline, attr, innerValue[0])
-                        mergeNodes(newObj, tempJsonObj as JsonObject)
-                    }
-    //                            }
-                    else -> {
-                        val newInnerArray = JsonArray()
-                        innerValue?.forEach { r -> newInnerArray.add(r) }
-                        newObj.add(k.key, newArray)
+                val kSize =if (k.value is Array<*>)
+                    (k.value as Array<*>).size
+                else (k.value as MutableMap<*, *>).size
+
+                val innerValue =
+                    if (k.value is Array<*>) {
+                                if ((k.value as Array<*>).size >= index)
+                                     (k.value as Array<*>).get(index - 1)
+                                else null
+                            } else
+                        (k.value as MutableMap<*, *>).get("$index")//(index-1)
+
+
+                val innerSize = if (innerValue == null) 0 else if (innerValue is Array<*>)
+                    (innerValue).size
+                else (innerValue as MutableMap<*, *>).values.size
+                //val innerValue = if (numberOfIterations >= index) k.value?.get(index - 1) else null
+//                if (innerValue == null)
+//                    newObj.add(k.key, null)
+//                else
+                    if (innerValue is Array<*>) {
+                    when (innerValue.size) {
+                        0 -> newObj.add(k.key.substring(1).split('.').last(), null)
+                        1 -> {
+                            val bline = k.key.split('.')
+                            val tempJsonObj = createJsonTree(bline, attr, (innerValue[0] as String))
+                            mergeNodes(newObj, tempJsonObj as JsonObject)
+                        }
+                        //                            }
+                        else -> {
+                            val newInnerArray = JsonArray()
+                            innerValue.forEach { r -> (r as Array<*>).forEach { rr -> newInnerArray.add(rr as String) } }
+                            val tempObj =  JsonObject()
+                            tempObj.add(k.key, newInnerArray)
+                            newObj.add(attr, tempObj)
+                        }
                     }
                 }
-
             }
             newArray.add(newObj.get(attr))
 
@@ -154,12 +203,9 @@ class HL72CSVTransform(val template: JsonObject) {
         return newArray
     }
 
-//    private fun navigateArray( hl7Message: HL7ParseUtils, jsonArray: JsonArray, ) {
-//
-//    }
 
-    private fun transformVariable(hl7Parser: HL7ParseUtils, substring: String, prim: String?): Array<out Array<String>>? {
-            val optionalValue = hl7Parser.getValue(prim, false)
+    private fun transformVariable(hl7Parser: HL7ParseUtils,  prim: String?): Array<out Array<String>>? {
+            val optionalValue = hl7Parser.getValue(prim)
             if (optionalValue.isDefined)
                 return optionalValue.get()
             return null
